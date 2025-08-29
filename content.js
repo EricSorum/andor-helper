6c0d284339a5602af655da3cc306ccbc9c202abd
Eric Sorum <ejsorum@gmail.com>
2025-08-29 12:20:04 -0500

    Removed unused code.

--- content.js
+++ content.js
@@ -14,28 +14,10 @@ function addLayover(movie) {
   if (movie.querySelector(".andor-layover-div")) {
     return;
   }
-// Create container div
-const div = document.createElement("div");
-div.classList.add("andor-layover-div");
-
-// Create image
-// const img = document.createElement("img");
-// img.src = chrome.runtime.getURL("images/andor-full.png");
-// img.classList.add("andor-layover-img");
-
-// Create text
-// const txt = document.createElement("div");
-// txt.classList.add("andor-layover-txt");
-// txt.innerText = "You could be watching Andor right now";
-
-// div.classList.add("andor-layover-txt");
-div.innerText = "You could be watching Andor right now";
-div.style.backgroundImage = `url(${chrome.runtime.getURL("images/andor-full.png")})`;
-
-// Build structure
-// div.appendChild(img);
-// div.appendChild(txt);
-movie.appendChild(div);
-
+  const div = document.createElement("div");
+  div.classList.add("andor-layover-div");
+  div.innerText = "You could be watching Andor right now";
+  div.style.backgroundImage = `url(${chrome.runtime.getURL("images/andor-full.png")})`;
+  movie.appendChild(div);
 }
 
