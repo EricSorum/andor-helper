aa8633678a9abaf5c89a003931451bb911bd9864
Eric Sorum <ejsorum@gmail.com>
2025-08-29 11:00:13 -0500

    DOM loaded trial and error

--- content.js
+++ content.js
@@ -1,8 +1,32 @@
-let movieSelector = ".boxart-container"
-const movies = document.querySelectorAll(movieSelector);
-movies.forEach(movie => addLayover(movie))
+console.log("content.js")
+
+const observer = new MutationObserver(() => {
+  document.querySelectorAll(".boxart-container").forEach(setupMovies);
+});
+
+observer.observe(document.body, {
+  childList: true,
+  subtree: true
+});
+
+
+function setupMovies() {
+  console.log("setup")
+  let movieSelector = ".boxart-container"
+  const movies = document.querySelectorAll(movieSelector);
+  console.log("map " + movies[0])
+  // movies.forEach(movie => addLayover(movie));
+
+  movies?.forEach(movie => removeImg(movie));
+}
+
+function removeImg(movie) {
+  console.log("removeImg")
+  movie.innerHTML = ""
+}
 
 function addLayover(movie) {
+  console.log("addLayover")
   const layoverImg = document.createElement("img");
   layoverImg.classList.add("andor-layover-img");
   movie.appendChild(layoverImg);
@@ -11,10 +35,5 @@ function addLayover(movie) {
   const layoverTxt = document.createElement("div");
   layoverTxt.classList.add("andor-layover-txt");
   layoverTxt.innerText = "You could be watching Andor instead";
-
-  chrome.scripting.insertCSS({
-    target: { tabId: currentTabId },
-    files: ['andor.css']
-  });
-
 }
+
